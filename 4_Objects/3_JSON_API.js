62200dc3fe3717fa8e70cd79b85ac9b08d65aa0b
adityapandey78 <adityapandey.2402@gmail.com>
2024-04-19 23:20:56 +0530

    .

---
+++ 4_Objects/3_JSON_API.js
@@ -0,0 +1,18 @@
+const course={
+    courseName:"Js in Hindi",
+    price:"999",
+    courseInstructer:"Hitesh OG!"
+}
+
+//course.courseInstructer => ese baar baar dot use krna make ths code tidy, so we can directly use the courseInstructure
+
+// const {courseInstructer}=course;
+// console.log(courseInstructer);
+
+//we can destructure the object using this method
+//idhar agar uska bada naam hai to uske dusra name bhi de skta hun
+const {courseInstructer:tr}=course;
+console.log(tr);
+
+
+
