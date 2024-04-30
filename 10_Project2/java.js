5cbc6b3c1a616413ae5f461aef740ec4726cdd06
adityapandey78 <adityapandey.2402@gmail.com>
2024-04-30 14:22:22 +0530

    .

--- 10_Project2/java.js
+++ 10_Project2/java.js
@@ -11,8 +11,18 @@ form.addEventListener('submit',function(e){
         results.innerHTML=`Please give a valid weight ${weight}`;
     }
     else{
-        const bmi=((weight*height)/10000).toFixed(2);//it results upto two digit
+        const bmi=((weight)/((height*height)/10000)).toFixed(2);//it results upto two digit
         //showing the result
-        results.innerHTML=`<span> ${bmi}</span>`
+        let status;
+        if(bmi<18.6){
+            status='Person is Underweight!'
+        }
+        else if(bmi>=18.6 && bmi<24.9){
+            status ='Person is Noraml'
+        }
+        else{
+            status='Person is overweight!'
+        }
+        results.innerHTML=`<span> ${bmi} and ${status} </span>`
     }
 });
