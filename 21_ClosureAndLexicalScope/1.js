f5e45de741c94e16124e1c4a97fb6695d65410c7
adityapandey78 <adityapandey.2402@gmail.com>
2024-05-18 03:45:03 +0530

    .

---
+++ 21_ClosureAndLexicalScope/1.js
@@ -0,0 +1,7 @@
+/*
+Lexical scope matlb us function ki outer scope kaha tk hai]
+bs that's it
+
+
+Closure tb bolte hain jb hum kisi function ko hi as a ref return kr dete hain jo uske lexical scope se baahar ho ya use ho raha ho
+*/
