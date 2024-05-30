3ee541ab9bfb5c9a53da207dd95bbe1b04f8a518
adityapandey78 <adityapandey.2402@gmail.com>
2024-05-30 12:08:37 +0530

    .

---
+++ Locomotive.js/1 project/script.js
@@ -0,0 +1,5 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true,
+});
+/*https://github.com/locomotivemtl/locomotive-scroll copied from here from  the section below smooth */
